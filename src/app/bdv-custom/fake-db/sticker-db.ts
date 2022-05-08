ce17e844a3b01f58be5b0286ad0549dcce26349d
Doan Van Binh <vanbinhdoan971@gmail.com>
2022-05-08 23:30:31 +0700

    add sticker img

---
+++ src/app/bdv-custom/fake-db/sticker-db.ts
@@ -0,0 +1,13 @@
+export interface Sticker {
+    id?: string;
+    name?: string;
+    imgUrl?: string;
+    textSearch?: string;
+    fatherId?: string;
+}
+
+export const stickerData: Sticker[] = [
+    {
+
+    }
+];
